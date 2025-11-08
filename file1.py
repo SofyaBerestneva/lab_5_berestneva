d15e4ecc11873be1d079cabe4951fa24b4ed773b
Sofya Berestneva <sofya.berestneva@bk.ru>
2025-11-08 16:29:12 +0300

    Внесение изменений в feature/some-change

--- file1.py
+++ file1.py
@@ -10,17 +10,17 @@ def even_bigger_10(numbers):
         else:
             ostatok.append(num)
     return {'result': result, 'ostatok': ostatok}
-
+# Функция сортироки
 def sort_results(results_dict):
     for key in results_dict:
         results_dict[key].sort()
     return results_dict
-# Генерируем список из 20 случайных чисел от 0 до 100 и добавляем их с помощью цикла
+
 numbers = [random.randint(0, 100) for _ in range(20)]
 print(f"Сгенерированные числа: {numbers}")
 results_dict = even_bigger_10(numbers)
 sorted_results = sort_results(results_dict)
 final_result = sorted_results['result']
 final_ostatok = sorted_results['ostatok']
-print(f"Чётные числа больше 10: {final_result}")
+print(f"Четные числа больше 10: {final_result}")
 print(f"Оставшиеся числа: {final_ostatok}")
