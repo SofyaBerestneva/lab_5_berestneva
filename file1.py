74755f516d9517a5fe219cc8fe89826915aa242a
Sofya Berestneva <sofya.berestneva@bk.ru>
2025-11-08 17:29:58 +0300

    Changing for conflict

--- file1.py
+++ file1.py
@@ -2,14 +2,14 @@ import random
 def even_bigger_num10(numbers):
     # result: Список чётных чисел больше 10.
     # ostatok: Список оставшихся чисел.
-    result = []
+    result23 = []
     ostatok = []
     for num in numbers:
         if num % 2 == 0 and num > 10:
-            result.append(num)
+            result23.append(num)
         else:
             ostatok.append(num)
-    return {'result': result, 'ostatok': ostatok}
+    return {'result': result23, 'ostatok': ostatok}
 # Функция сортироки
 def sort_results(results_dict):
     for key in results_dict:
